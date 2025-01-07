5f8598d5e711c5395fc24d6b813bc04825c9740a
zivcoh12 <zivcoh12@gmail.com>
2025-01-07 17:47:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pandas as pd
 parkinsons_df = pd.read_csv('parkinsons.csv')
 parkinsons_df= parkinsons_df.dropna()
 parkinsons_df.head()
-selected_features = ['PPE', 'RPDE']
+selected_features = ['PPE', 'DFA']
 target= ['status']
 x=parkinsons_df[selected_features]
 y=parkinsons_df[target]
@@ -13,7 +13,7 @@ scaler = MinMaxScaler()
 x_scaled = scaler.fit_transform(x)
 
 from sklearn.model_selection import train_test_split
-x_train, x_test, y_train, y_test = train_test_split(x_scaled, y, test_size=0.15, random_state=42)
+x_train, x_test, y_train, y_test = train_test_split(x_scaled, y, test_size=0.2, random_state=42)
 
 from sklearn.neighbors import KNeighborsClassifier
 knn = KNeighborsClassifier(n_neighbors=5)
